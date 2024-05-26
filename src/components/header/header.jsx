4ada01f9bd956c72132702845d7f315e5f103e1c
Shaheen <avrostu@gmail.com>
2024-05-26 23:41:24 +0600

    DESKTOP NAVIGATION COMPLETED

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,5 +1,7 @@
 import "./header.scss";
-
+// Import FontAwesome components and icons
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 // HEADER
 
 function Header() {
@@ -8,44 +10,44 @@ function Header() {
       <header>
         <div className="row">
           <nav>
-            <a className="logo text-blueish fw-700 fs-400" href="/">
-              CapstoneBD
+            <a className="logo text-neutral fw-700 fs-450" href="/">
+              CLOTHX
               <sup>
-                <span className="text-redish">TM</span>
+                <span className="text-neutral">TM</span>
               </sup>
             </a>
-            <ul className="nav-center">
+            <ul className="nav-center text-cap fw-700">
               <li>
-                <a href="#features" className="text-neutral">
-                  Features
+                <a href="#home" className="text-neutral">
+                  Home
                 </a>
               </li>
               <li>
-                <a href="#links" className="text-neutral">
-                  Links
+                <a href="#men" className="text-neutral">
+                  Men
                 </a>
               </li>
               <li>
-                <a href="#newsletter" className="text-neutral">
-                  Newsletter
+                <a href="#women" className="text-neutral">
+                  Women
                 </a>
               </li>
               <li>
-                <a href="#links" className="text-neutral">
-                  About
+                <a href="#kids" className="text-neutral">
+                  Kids
                 </a>
               </li>
             </ul>
 
             <ul className="nav-right">
               <li>
-                <a href="#" className="text-neutral">
-                  Support
+                <a href="#" className="text-neutral btn btn-cart">
+                <FontAwesomeIcon icon={faCartShopping} />
                 </a>
               </li>
               <li>
                 <a href="#">
-                  <button className="border btn btn-sign-in text-neutral">
+                  <button className="btn btn-sign-in text-neutral fw-700">
                     Sign In
                   </button>
                 </a>
